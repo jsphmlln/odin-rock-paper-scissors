ad7a15675bbf1c39ec05ef3059f6c4b870ce1d43
jsphmlln <205103278+jsphmlln@users.noreply.github.com>
2025-07-21 14:58:02 +0800

    initial finished version; added playRound loop

--- javascript.js
+++ javascript.js
@@ -23,5 +23,52 @@ function getHumanChoice(){
     }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+const cw = "Computer Wins";
+const pw = "Player Wins";
+
+function playRound(){
+    console.log(`Player: ${humanScore}  Computer: ${computerScore}`)
+    let player = getHumanChoice();
+    let computer = getComputerChoice();
+
+    console.log(`Player: ${player}`)
+    console.log(`Computer: ${computer}`)
+    
+    if (player === "ROCK"){
+        if (computer === "PAPER"){
+            console.log(cw);
+            computerScore ++;
+        }
+        if (computer === "SCISSORS"){
+            console.log(pw);
+            playerScore ++;
+        }
+    } else if (player === "PAPER"){
+        if (computer === "SCISSORS"){
+            console.log(cw);
+            computerScore ++;
+        }
+        if (computer === "ROCK"){
+            console.log(pw);
+            playerScore ++;
+        }
+    } else if (player === "SCISSORS"){
+        if (computer === "ROCK"){
+            console.log(cw);
+            computerScore ++;
+        }
+        if (computer === "PAPER"){
+            console.log(pw);
+            playerScore ++;
+        }
+    } else {
+        console.log("Draw.");
+    }
+
+    console.clear();
+    playRound();
+}
+
+playRound();
